a40ca97060565d8430f24e0a63ec57563c5ecc71
Hridoy Mozumder <hrrarya6@gmail.com>
2025-05-04 23:15:01 +0600

    remove: unnecessary initial packages, keep it simple

--- react_app/index.js
+++ react_app/index.js
@@ -1,21 +1,14 @@
-import React, { createContext } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from "react";
+import { createRoot } from "react-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { Provider } from "react-redux";
 import App from "App";
-import store from "redux/store";
 import { createHooks } from "@wordpress/hooks";
 window.main_plugin_hooks = createHooks();
 
 document.addEventListener("DOMContentLoaded", function () {
-	const body = document.getElementById("TEXT_DOMAIN-body");
-	const root = createRoot(body);
+  const body = document.getElementById("TEXT_DOMAIN-body");
+  const root = createRoot(body);
 
-	root.render(
-		<Provider store={store}>
-			<Router>
-				<App />
-			</Router>
-		</Provider>
-	);
+  root.render(<App />);
 });
