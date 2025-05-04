f29833d7195642d4644b6a7b3cd9eef0352e059a
Hridoy Mozumder <hrrarya6@gmail.com>
2025-05-04 23:15:24 +0600

    YARN BUILD

---
+++ assets/js/TEXT_DOMAIN.core.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-hooks'), 'version' => '9f7babb94bf510786680');
--- assets/js/main-plugin.core.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '1513b75c5bb25b2b7bd6');
